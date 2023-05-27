5dfa17d706f7d2f0883a30667dab2cf63b616124
Victor <victoluolatunji@gmail.com>
2023-05-27 04:42:02 +0100

    codebase✔

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <h1 className="text-4xl font-semibold">VickyJay</h1>
+    </div>
+  )
+}
+
+export default App
